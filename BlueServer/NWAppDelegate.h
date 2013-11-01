f68c6d6cca0022343ed565c1fcbbead19da06056
Nicholas Wargnier <nwargnier@gmail.com>
2013-10-31 17:45:34 -0700

    Initial Commit

---
+++ BlueServer/NWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NWAppDelegate.h
+//  BlueServer
+//
+//  Created by Nicholas Wargnier on 10/31/13.
+//  Copyright (c) 2013 Nicholas Wargnier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlueServerTests/BlueServerTests.m
@@ -0,0 +1,34 @@
+//
+//  BlueServerTests.m
+//  BlueServerTests
+//
+//  Created by Nicholas Wargnier on 10/31/13.
+//  Copyright (c) 2013 Nicholas Wargnier. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BlueServerTests : XCTestCase
+
+@end
+
+@implementation BlueServerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
